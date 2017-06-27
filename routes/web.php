5ef0515b38c01c32b459a50ce80b50614b595f1f
haonx <qtvhao@gmail.com>
2017-06-27 22:12:45 +0700

    set random 3 properties to new Gag

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,11 @@ Route::post('/', function(){
     /** @var UploadedFile $user_file_media */
     $user_file_media = request()->user_file_media;
     $path = $user_file_media->store('public/images');
-    echo $path;
     //image was stored to 'storage/app/public/images' directory
     $new_gag = new Gag();
+    $new_gag->type = str_random(10);
+    $new_gag->media_source = $path;
+    $new_gag->title = str_random(10);
     $new_gag->save();
     dd(Gag::all());
 });
