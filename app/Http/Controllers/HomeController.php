bb46b378511b73f2bc00d5d4ac2140dbd8c2c129
haonx <qtvhao@gmail.com>
2017-07-06 20:30:37 +0700

    update phpDoc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,11 +17,6 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $gag_id = \request()->route('gag');
