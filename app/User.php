ba60678f3cd27219b2fa796b417821c03f0b559a
haonx <qtvhao@gmail.com>
2017-07-07 19:19:34 +0700

    set relation 'votes' of User to Gag

--- app/User.php
+++ app/User.php
@@ -30,4 +30,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function votes()
+    {
+        return $this->belongsToMany(Gag::class, 'user_votes');
+    }
 }
